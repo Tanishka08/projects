edb0774a3bdb4e83ff872b8492a791b3d364ba36
Tanishka08 <59464106+Tanishka08@users.noreply.github.com>
2020-02-04 02:11:47 -0800

    Add files via upload

---
+++ Fortune/src/main/java/com/mySpringPlatform/Fortune/App.java
@@ -0,0 +1,13 @@
+package com.mySpringPlatform.Fortune;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
