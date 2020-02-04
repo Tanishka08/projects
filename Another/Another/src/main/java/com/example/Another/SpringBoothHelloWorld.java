b48bee5b1795737aee3685414f2b53e37e4c0c46
Tanishka08 <59464106+Tanishka08@users.noreply.github.com>
2020-02-04 02:18:43 -0800

    Add files via upload

---
+++ Another/Another/src/main/java/com/example/Another/SpringBoothHelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.Another;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoothHelloWorld 
+{
+	
+
+		public static void main(String[] args) {
+			SpringApplication.run(SpringBoothHelloWorld.class, args);
+		}
+
+}
